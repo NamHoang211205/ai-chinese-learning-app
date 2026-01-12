5d6d11f5484921dece83d33b403ac090c35afbc2
Namhoang211205 <namhoang211205@gmail.com>
2026-01-11 19:35:32 -0500

    Fixing System

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
-import { Button } from "@/components/ui/button"
+import Cta from "@/components/Cta"
+import CompanionCard from "@/components/ui/CompanionCard"
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-       <h1 className='text-2xl underline'> Welcome To My Learning App </h1>
-       <Button> Let's get started </Button>
-    </div>
+    <main>
+       <h1 className='text-2xl underline'> popular companions </h1>
+       <section className="home-section" >
+        <CompanionCard />
+        <CompanionCard />
+        <CompanionCard />
+       </section>
+
+       <section className="home-section">
+        <CompanionCard />
+        <Cta />
+       </section>
+    </main>
   )
 }
 export default Page
