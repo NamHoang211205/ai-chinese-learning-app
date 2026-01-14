ee4671036f5b19689d00567d05883845891ace38
Namhoang211205 <namhoang211205@gmail.com>
2026-01-14 14:26:46 -0500

    Adding features for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Cta from "@/components/Cta"
 import CompanionCard from "@/components/ui/CompanionCard"
+import CompanionList from "@/components/ui/CompanionList"
 import React from 'react'
 
 const Page = () => {
@@ -7,13 +8,35 @@ const Page = () => {
     <main>
        <h1 className='text-2xl underline'> popular companions </h1>
        <section className="home-section" >
-        <CompanionCard />
-        <CompanionCard />
-        <CompanionCard />
+        <CompanionCard
+        id="123"
+        name="Nam Hoang"
+        topic="AI"
+        subject="Chinese"
+        duration={30}
+        color="#ffda6e" 
+        />
+        <CompanionCard 
+        id="456"
+        name="Nam Hoang"
+        topic="Writing"
+        subject="Chinese"
+        duration={30}
+        color="#e5d0ff" 
+        />
+        
+        <CompanionCard 
+        id="789"
+        name="Nam Hoang"
+        topic="Speaking"
+        subject="Chinese"
+        duration={30}
+        color="#BDE7FF" 
+        />
        </section>
 
        <section className="home-section">
-        <CompanionCard />
+        <CompanionList />
         <Cta />
        </section>
     </main>
--- components/ui/CompanionCard.tsx
+++ components/ui/CompanionCard.tsx
@@ -1,8 +1,37 @@
+import { Link } from 'lucide-react';
+import Image from 'next/image';
 import React from 'react'
+interface CompanionCardProps {
+  id: string;
+  name: string;
+  topic: string;
+  subject: string;
+  duration: number;
+  color: string;
+}
 
-const CompanionCard = () => {
+const CompanionCard = ({id, name, topic, subject, duration, color}:
+  CompanionCardProps) => {
   return (
-    <div>CompanionCard</div>
+    <article className='companion-card' style={{backgroundColor: color}}>
+      <div className='flex justify-between items-center'>
+        <div className='subject-badge'>{subject}</div>
+        <button className='companion-bookmark'>
+          <Image src="/icons/bookmark.svg" alt="Bookmark" width={12.5} height={15} />
+        </button>
+      </div>
+
+      <h2 className="text-2xl font-bold">{name}</h2>
+      <p className='text-sm'>{topic}</p>
+      <div className='flex items-center gap-2'>
+        <Image src='/icons/clock.svg' alt="duration" width={15} height={15} />
+        <p className='text-sm'>{duration} mins</p>
+      </div>
+
+      <Link href={`/companion/${id}`} className='w-full'>
+        <button className='btn-primary w-full just'>Start Session</button>
+      </Link>
+    </article>
   )
 }
 
