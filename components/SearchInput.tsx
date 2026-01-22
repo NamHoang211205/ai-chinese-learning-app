9daa44256461568a1a6f3ace7ab943fce3238317
Namhoang211205 <namhoang211205@gmail.com>
2026-01-21 20:20:57 -0500

    Finished Search Filter

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,8 +1,9 @@
 
 'use client';
 import { usePathname, useSearchParams, useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import { formUrlQuery, removeKeysFromUrlQuery } from '@jsmastery/utils';
 
 const SearchInput = () => {
     const pathname = usePathname();
@@ -11,12 +12,34 @@ const SearchInput = () => {
     const query = searchParams.get('topic') || '';
 
     const [searchQuery, setSearchQuery] = useState('');
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+           if(searchQuery) {
+            const newUrl = formUrlQuery({
+                params: searchParams.toString(),
+                key: 'topic',
+                value: searchQuery,
+            });
+            router.push(newUrl, { scroll: false });
+        } else {
+            if (pathname === '/companions') {
+                const newUrl = removeKeysFromUrlQuery({
+                    params: searchParams.toString(),
+                    keysToRemove: ['topic'],
+                });
+                router.push(newUrl, { scroll: false });
+            }
+        }
+        }, 500)
+        
+    }, [searchQuery, router, searchParams,pathname]);
     return (
         <div className='relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit'>
             <Image src="/icons/search.svg" alt="search" width={15} height={15} />
             <input
                 type="text"
                 placeholder='Search by topic...'
+                className ='outline-none'
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
             />
