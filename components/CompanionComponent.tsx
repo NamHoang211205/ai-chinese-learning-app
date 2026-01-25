0d52dfde85df5811849d9eafb380a1a6a2d40138
Namhoang211205 <namhoang211205@gmail.com>
2026-01-25 18:39:58 -0500

    Adding Voice Mode

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import { cn, getSubjectColor } from '@/lib/utils'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { vapi } from '@/lib/vapi.sdk';
+import Image from 'next/image';
+import Lottie, { LottieRefCurrentProps } from 'lottie-react';
+import soundwaves from '@/constants/soundwaves.json';
 
 enum CallStatus {
     INACTIVE = 'INACTIVE',
@@ -13,6 +16,19 @@ enum CallStatus {
 const CompanionComponent = ({companionId, subject, topic, name, userName, userImage, style, voice}: CompanionComponentProps) => {
     const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
     const [isSpeaking, setIsSpeaking] = useState<boolean>(false);
+    const [isMuted, setIsMuted] = useState<boolean>(false);
+    const lottieRef = useRef<LottieRefCurrentProps>(null);
+
+    useEffect(() => {
+        if(lottieRef) {
+            if(isSpeaking) {
+                lottieRef.current?.play()
+            } else {
+                lottieRef.current?.stop()
+            }
+        }
+    }, [isSpeaking, lottieRef])
+    
     useEffect(() => {
         const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
         const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
@@ -36,8 +52,18 @@ const CompanionComponent = ({companionId, subject, topic, name, userName, userIm
             vapi.off('speech-start', onSpeechStart);
             vapi.off('speech-end', onSpeechEnd);
         }
-        
     }, []);
+
+    const toggleMicrophone = () => {
+        const isMuted = vapi.isMuted();
+        vapi.setMuted(!isMuted);
+        setIsMuted(!isMuted);
+    }
+
+    const HandleCall = async () => {}
+
+    const handleDisconnect = async () => {}
+
   return (
     <section className='flex flex-col h-[70vh]'>
         <section className='flex gap-8 max-sm:flex-col'>
@@ -50,9 +76,45 @@ const CompanionComponent = ({companionId, subject, topic, name, userName, userIm
                                 callStatus === CallStatus.FINISHED || callStatus === CallStatus.INACTIVE ? 'opacity-1001' : 'opacity-0', 
                                 callStatus === CallStatus.CONNECTING && 'opacity-100 animate-pulse'
                                 )}>
+                                    <Image src={`/icons/${subject}.svg`} alt={subject} width={150} height={150} className='max-sm:w-fit' />
+                    </div>
 
+                    <div className={cn('absolute transition-opacity duration-1000', callStatus === CallStatus.ACTIVE ? 'opacity-100' : 'opacity-0')}>
+                        <Lottie 
+                            lottieRef={lottieRef} 
+                            animationData={soundwaves}
+                            autoPlay={false}
+                            className='companion-lottie'
+                            />
                     </div>
                 </div>
+                <p className='font-bold text-2xl'>{name}</p>
+            </div>
+            <div className='user-section'>
+                <div className='user-avatar'>
+                    <Image src={userImage} alt={userName} width={130} height={130} className='rounded-lg' />
+                    <p className='font-bold text-2xl'>{userName}</p>
+                </div>
+                <button className='btn-mic' onClick={toggleMicrophone}>
+                    <Image 
+                        src={isMuted ? '/icons/mic-off.svg' : '/icons/mic-on.svg'} 
+                        alt={isMuted ? 'Unmute microphone' : 'Mute microphone'} 
+                        width={36} 
+                        height={36} 
+                    />
+                    <p className='max-sm:hidden'>
+                        {isMuted ? 'Unmute Microphone' : 'Mute Microphone'}
+                    </p>
+                </button>
+                <button className={cn('rounded-lg py-2 cursor-pointer transition-colors w-full text-white', 
+                    callStatus === CallStatus.ACTIVE ? 'bg-red-700' : 'bg-primary', 
+                    callStatus === CallStatus.CONNECTING && 'animate-pulse')} 
+                    onClick={callStatus === CallStatus.ACTIVE? handleDisconnect : HandleCall}>
+                    {callStatus === CallStatus.ACTIVE
+                    ? 'End Session' 
+                    : callStatus === CallStatus.CONNECTING ? 'Connecting...'
+                    : 'Start Session'}
+                </button>
             </div>
         </section>
         </section>
