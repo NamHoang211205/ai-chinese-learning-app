51a4ff49220c864dafde3a6503a003f93577e5ad
Namhoang211205 <namhoang211205@gmail.com>
2026-01-27 15:30:51 -0500

    Finished Sentry

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -33,7 +33,12 @@ const CompanionComponent = ({companionId, subject, topic, name, userName, userIm
     
     useEffect(() => {
         const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
-        const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
+
+        const onCallEnd = () => {
+            setCallStatus(CallStatus.FINISHED);
+            // addToSessionHistory(companionId);
+        };
+
         const onMessage = ( message: Message) => {
             if(message.type === 'transcript' && message.transcriptType === 'final') {
                 const newMessage = {role: message.role, content: message.transcript}
